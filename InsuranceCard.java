4bb6088d7043305d901e0f2a9bb19f6345042d93
danle0986 <153896588+danle0986@users.noreply.github.com>
2024-04-03 10:09:49 +0700

    Add files via upload
    Claim class

---
+++ InsuranceCard.java
@@ -0,0 +1,11 @@
+package Assignment1;
+
+import java.util.Date;
+
+public class InsuranceCard
+{
+    private String cardNum;
+    private String cardHolder;
+    private String policyHolder;
+    private Date expireDate;
+}
