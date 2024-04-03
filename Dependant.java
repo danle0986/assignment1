682e1a6beecfce911b286c069730c9a572967962
danle0986 <153896588+danle0986@users.noreply.github.com>
2024-04-03 20:43:56 +0700

    Update Dependant.java

--- Dependant.java
+++ Dependant.java
@@ -1,6 +1,10 @@
 package Assignment1;
 
+import java.util.List;
+
 public class Dependant extends Customer
 {
-    public Dependant(){}
+    public Dependant(String id, String fullName, InsuranceCard card, List<Claim> claimList) {
+        super(id, fullName, card, claimList);
+    }
 }
