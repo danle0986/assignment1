074ef6506274d87fdb1d2fac68d23b39732b3936
danle0986 <153896588+danle0986@users.noreply.github.com>
2024-04-03 20:44:18 +0700

    Update InsuranceCard.java

--- InsuranceCard.java
+++ InsuranceCard.java
@@ -8,4 +8,28 @@ public class InsuranceCard
     private String cardHolder;
     private String policyHolder;
     private Date expireDate;
+
+    public InsuranceCard() {
+        this.cardNum = null;
+        this.cardHolder = null;
+        this.policyHolder = null;
+        this.expireDate = null;
+    }
+    public InsuranceCard(String cardNum, String cardHolder, String policyHolder, Date expireDate) {
+        this.cardNum = cardNum;
+        this.cardHolder = cardHolder;
+        this.policyHolder = policyHolder;
+        this.expireDate = expireDate;
+    }
+
+    @Override
+    public String toString() {
+        return "InsuranceCard{" +
+                "cardNum='" + cardNum + '\'' +
+                ", cardHolder='" + cardHolder + '\'' +
+                ", policyHolder='" + policyHolder + '\'' +
+                ", expireDate=" + expireDate +
+                '}';
+    }
 }
+
