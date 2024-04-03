ab9f0e43dabc81ef96c68696a2ac4ef2db73665f
danle0986 <153896588+danle0986@users.noreply.github.com>
2024-04-03 20:44:35 +0700

    Update PolicyHolder.java

--- PolicyHolder.java
+++ PolicyHolder.java
@@ -1,8 +1,14 @@
 package Assignment1;
 
+import java.util.List;
 import java.util.Set;
 
 public class PolicyHolder extends Customer
 {
     private Set<Dependant> dependantList;
+
+    //constructor for testing without dependantList
+    public PolicyHolder(String id, String fullName, InsuranceCard card, List<Claim> claimList) {
+        super(id, fullName, card, claimList);
+    }
 }
