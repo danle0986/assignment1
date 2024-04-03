eb1f72adf2ad9cfd506ca176517dd5194e212b80
danle0986 <153896588+danle0986@users.noreply.github.com>
2024-04-03 11:16:00 +0700

    Add files via upload
    Claim class

---
+++ Claim.java
@@ -0,0 +1,16 @@
+package Assignment1;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Claim
+{
+    private String id;
+    private Date claimDate;
+    private String insuredPerson;
+    private ArrayList<String> documentList;
+    private double claimAmount;
+    private String Status;
+    private String receiverBankInfo;
+
+}
