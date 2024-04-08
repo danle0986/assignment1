04ebb0a2f503ff2d703c7c9d3a136bfa9b36b382
danle0986 <153896588+danle0986@users.noreply.github.com>
2024-04-08 17:36:44 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 package Assignment1;
 
+/** 
+* @author <Le Duc Anh Pham - s4010986> 
+*/ 
+
 public class Main {
     static public void main(String args[]) {
         InsuranceSystem system = new InsuranceSystem();
